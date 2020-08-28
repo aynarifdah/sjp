973e05d59dad52aae5a69bcff5d8bbeb588b5856
Jira SC <jira.devsc@gmail.com>
2020-08-28 13:32:19 +0700

    merge dengan shintia2

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -48,11 +48,7 @@ class Auth extends CI_Controller {
 	    $password 	= $this->input->post('password'); 
 	    $user 		= $this->M_login->readBy($username);
 	    // var_dump($user);die;
-<<<<<<< HEAD
-            //  echo  $this->encryption->encrypt($password);die; 
-=======
           //   echo  $this->encryption->encrypt($password);die; 
->>>>>>> origin/shintia
           // echo "<br><br>";
             // echo $this->encryption->decrypt($user->password);
            // echo $password;die;
