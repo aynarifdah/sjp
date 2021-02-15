1b1503fbf163cb5e5b4d9343c4ca08d5b01115c1
unknown <rizaldirifki7@.com>
2021-02-15 17:36:29 +0700

    Naik Versi

---
+++ application/views/dinkes/cetak.php
@@ -0,0 +1,159 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Surat Jaminan Pelayanan</title>
+    <style>
+        @font-face {
+            font-family: Arial;
+            font-style: normal;
+            font-weight: normal;
+            src: url(/application/third_party/dompdf/lib/fonts/arial.ttf) format("truetype"));
+        }
+
+        body {
+            margin-top: 0px;
+            margin-left: 10px;
+        }
+
+        #kop {
+            margin-bottom: 30px;
+        }
+
+        .a {
+            display: inline-block;
+            width: 70px;
+            font-size: 16px;
+        }
+
+        .b {
+            display: inline-block;
+            width: 20px;
+            font-size: 16px;
+        }
+
+        .c {
+            display: inline-block;
+            width: 400px;
+            font-size: 16px;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th,
+        td {
+            text-align: left;
+            padding: 5px;
+        }
+
+        .content {
+            font-family: Arial !important;
+            font-size: 12px;
+            text-align: justify;
+            margin-left: 100px;
+            margin-right: 30px;
+        }
+
+        .right {
+            float: right;
+        }
+
+        .left {
+            float: left;
+        }
+    </style>
+</head>
+
+<body>
+    <img src=' . $img_kop . ' alt="" id="kop" width="100%">
+    <br>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    Depok<br>
+
+
+    <span class="a">Nomor</span> <span class="b">:</span><span class="c"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kepada :<br>
+
+    <span class="a">Lamp</span> <span class="b">:</span><span class="c">1 (satu) berkas</span>Yth. Direktur <br>
+    <span class="a">Hal</span> <span class="b">:</span> <span class="c">Surat Jaminan Pelayanan</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Di
+
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Tempat<br>
+
+
+    <br>
+    <div class="row">
+        <div class="col-lg-12">
+
+            Dari hasil penelitian kami atas surat-surat dari :
+            <table class="table table-borderless table-sm">
+                <tbody>
+                    <tr>
+                        <td style="width: 30%">Nama Pasien</td>
+                        <td style="width: 5%">:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%">Tanggal Lahir</td>
+                        <td style="width: 5%">:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%">Jenis Kelamin</td>
+                        <td style="width: 5%">:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%">Tgl. Mulai Rawat</td>
+                        <td style="width: 5%">:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%">Alamat</td>
+                        <td style="width: 5%">:</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table><br>
+
+            Ternyata pasien tersebut memenuhi syarat :
+            <table class="table table-borderless table-sm">
+                <tbody>
+                    <tr>
+                        <td style="width: 30%">Dirawat di</td>
+                        <td style="width: 5%">:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%">Dilakukan</td>
+                        <td style="width: 5%">:</td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td style="width: 30%">Diagnosa sementara</td>
+                        <td style="width: 5%">:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%">Diberikan jaminan</td>
+                        <td style="width: 5%">:</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td style="width: 30%">Jaminan</td>
+                        <td style="width: 5%">:</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <p>Atas biaya Pemerintah Kota Depok dengan ketentuan yang berlaku. Biaya tersebut agar diajukan oleh Rumah Sakit secara kolektif sebelum tanggal 10 pada bulan berikutnya.</p>
+    <!-- <img src=' . $ttd . ' alt="" id="kop" width="280" height="161" align="right"> -->
+
+</body>
+
+</html>
