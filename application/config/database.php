ad1d55cf0ab82075bd0b62a5c5abeb6ce94908cc
Shintia <shintiamutia28@gmail.com>
2021-10-11 11:14:32 +0700

    db

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => '18.136.123.139',
+	'username' => 'scdev',
+	'password' => 'p1n4ng9A',
 	'database' => 'sjp',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
