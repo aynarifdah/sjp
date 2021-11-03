d4bd2629f814c4f7ff17b7194c9151077965cb94
Shintia <shintiamutia28@gmail.com>
2021-11-03 20:34:58 +0700

    update

--- application/config/database.php
+++ application/config/database.php
@@ -75,12 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'sjpp',
-
+    'hostname' => 'localhost',
+	'username' => 'sjp',
+	'password' => 'x]&xf_a(',
+	'database' => 'sjp',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
