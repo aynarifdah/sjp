5cf7a97ef9d5bb97b0a3a2d20156dc30caecf819
root <root@sjp.depok.go.id>
2022-08-13 17:25:08 +0000

    checkpoint

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -48,7 +48,7 @@ data-open="click" data-menu="vertical-menu" data-col="2-columns">
               </a>
               <div class="dropdown-menu dropdown-menu-right">
                 <div class="dropdown-divider"></div><a class="dropdown-item" href="<?php echo base_url('Auth/logout');?>"><i class="ft-power"></i> Logout</a>
-                <div class="sso-depok-logout-btn"></div>
+                <!-- <div class="sso-depok-logout-btn"></div> -->
               </div>
             </li>
           </ul>
