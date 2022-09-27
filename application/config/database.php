c4e29f40169a05bfdd2a33fa90d21a94614ec001
Jira SC <jira.devsc@gmail.com>
2022-09-27 15:16:08 +0700

    config

--- application/config/database.php
+++ application/config/database.php
@@ -141,9 +141,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-    'hostname' => 'localhost',
-	'username' => 'sjp',
-	'password' => 'x]&xf_a(',
+    'hostname' => '18.136.123.139',
+	'username' => 'scdev',
+	'password' => 'p1n4ng9A',
 	'database' => 'sjp',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
