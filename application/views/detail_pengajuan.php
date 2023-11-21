4010e5eb2b08d80a52fbe82d931b577aa75ce35d
unknown <iqbal.radjasa@gmail.com>
2023-11-21 17:06:51 +0700

    Before merge to feature/integrasi-sitpas

--- application/views/detail_pengajuan.php
+++ application/views/detail_pengajuan.php
@@ -752,7 +752,7 @@
                    <div class="note">
                      <p> <strong>Catatan Dokumen:</strong><span class="catatan"><?php echo $key['feedback']; ?></span></p>
                    </div>
-                   <section id="image-gallery" class="card">  
+                   <section id="image-gallery" class="card">
                      <div class="card-content">
                        <div class="card-body  my-gallery" itemscope itemtype="http://schema.org/ImageGallery">
                          <div class="row">
@@ -771,11 +771,6 @@
                                      <p class="mt-1 text-sm text-center" style="font-size: 12px;" id="name_file_pdf"><?= $att['attachment'] ?></p>
                                    </a>
                                  </div>
-                               <?php else : ?>
-                                 <figure class="col-lg-6 col-md-6 col-12">
-                                   <a class="example-image-link" href="<?php echo base_url() ?>uploads/dokumen/<?php echo $att['attachment'] ?>" data-lightbox="Dokumen Pesyaratan">
-                                     <img class="example-image" style="width: 80%; height: auto;" src="<?php echo base_url() ?>uploads/dokumen/<?php echo $att['attachment'] ?>" alt="" /></a>
-                                 </figure>
                                <?php endif; ?>
 
 
