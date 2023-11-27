1460e961019e8ed6a2fa29e06a9a540980dd66fd
unknown <rifqyzanuar5@gmail.com>
2023-11-27 14:25:53 +0700

    hide tte dinkes rs

--- application/controllers/Rs.php
+++ application/controllers/Rs.php
@@ -1643,6 +1643,18 @@ public function proses_entry_klaim()
       <div class="info">
       <p>Atas biaya Pemerintah Kota Depok dengan ketentuan yang berlaku. Biaya tersebut agar diajukan oleh<br> Rumah Sakit secara kolektif sebelum tanggal 10 pada bulan berikutnya.</p>
       </div>
+      <img src=' . $ttd . ' alt="" id="kop" width="310" height="140" align="right">
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br><br><br><br><br><br>
+      <div class="footer" style="margin-bottom:0">
+      <center><p><em>Dokumen ini telah ditandatangani secara elektronik menggunakan sertifikat elektronik yang diterbitkan oleh Balai<br> Sertifikasi Elektronik (BSrE), Badan Siber dan Sandi Negara.</em></p></center>
+      </div>
+
 
       </body></html>';
         return $html;
