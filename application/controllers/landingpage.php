68c7ab5c57fd003234695be672f637ceca09221a
unknown <rifqyzanuar5@gmail.com>
2023-12-21 13:28:24 +0700

    push Landingpage

--- application/controllers/landingpage.php
+++ application/controllers/landingpage.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class landingpage extends CI_Controller
+class Landingpage extends CI_Controller
 {
 
     public function __construct()
