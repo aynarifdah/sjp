2541970736ebb043242d9094fc64da512f0842a2
unknown <rifqyzanuar5@gmail.com>
2023-12-28 08:07:35 +0700

    config dummy

--- application/config/database.php
+++ application/config/database.php
@@ -140,23 +140,24 @@ $query_builder = TRUE;
 // );
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'   => '',
     'hostname' => 'localhost',
-	'username' => 'sjpuser',
-	'password' => 'GzGqa)f)87]@u{)^?+9}arL',
-	'database' => 'sjp',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
+    'username' => 'userscdev',
+    'password' => '9[Sr!2+uu%^A4eJWsYd_&@9H*',
+    'database' => 'sjp',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => TRUE,
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+
 );
