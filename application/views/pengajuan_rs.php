bc24b8b76cd58aebf9294f0f69ee799c8036993f
unknown <rifqyzanuar5@gmail.com>
2025-01-04 15:03:39 +0700

    tambah kondisi di tangguhkan di pengajuan rs

--- application/views/pengajuan_rs.php
+++ application/views/pengajuan_rs.php
@@ -324,6 +324,10 @@
               // $('.statuspengajuan').addClass('bg-success');
               // return row.status_pengajuan;
               return '<div class="badge bg-primary " style="font-size: 14px;">' + row.status_pengajuan + '</div>'
+            } else if (data == 9) {
+              // $('.statuspengajuan').addClass('bg-success');
+              // return row.status_pengajuan;
+              return '<div class="badge bg-warning " style="font-size: 14px;">' + row.status_pengajuan + '</div>'
             } else {
               // $('.statuspengajuan').addClass('bg-success');
               // return row.status_pengajuan;
